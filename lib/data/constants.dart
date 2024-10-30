f50201fcc13856180ddf4828c757f49426f6e665
Ahmed Kishk <ahmed.vet_2906@vet.kfs.edu.eg>
2024-10-30 20:08:35 +0300

    Invalidated supabase anon key and used compile-time constants instead

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,8 +1,4 @@
 class Constants {
-  static const supabaseUrl = 'https://darxteoxuvjqshylljgf.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc'
-      '3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhcnh0ZW94dXZqcXNo'
-      'eWxsamdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgxMDM'
-      '5MjQsImV4cCI6MjA0MzY3OTkyNH0.CGMtI5awxZTh0xeLuv'
-      '5Dz8jDSJdYW6sE-fU1HzLqK7w';
+  static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
 }
